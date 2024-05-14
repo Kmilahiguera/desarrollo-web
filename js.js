a0dd69edee7bde3af7153439368e90c530b1f5f1
kmila <camila115011@gmail.com>
2024-05-14 15:14:34 -0500

    1.03

---
+++ js.js
@@ -0,0 +1,20 @@
+
+for (i=0; i<3; i++)
+{
+alert("Hola!!!") //se puede usar coma o no, si las pones ponlas siempre si no no :D
+}
+
+//función en js
+
+function escribir()
+{
+    document.write("dajashjads") //al llamar esto se escribe esto
+}
+
+function numeros()
+{
+for (i=1; i<11; i++)
+{
+    document.write(i,", ")
+}
+}
